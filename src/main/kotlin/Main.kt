68b5788e503e19229459c0b08d8552777b3e8c59
김정민 <kjungmin0601@gmail.com>
2023-06-06 19:51:20 +0900

    feature main 서버 추가
    - README 1일차 공부 내용 업데이트
    - WebServer.kt 웹 서버와 관련 된 내용 들어갈 파일
    - Main.kt 프로젝트 시작점

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
+import org.apache.catalina.startup.Tomcat
+import java.io.File
+
 fun main() {
-    println("Hello world")
+    val webAppDirection = "webapps/"
+    val tomcat = Tomcat()
+    tomcat.setPort(8080)
+    tomcat.addWebapp("/", File(webAppDirection).absolutePath)
+    WebServer.log.info("configuring app with base dir: {}",
+        File("./${webAppDirection}").absolutePath)
+    tomcat.start()
+    tomcat.server.await()
 }
