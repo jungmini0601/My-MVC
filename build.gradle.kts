4b353426b916585dd1665a1044c11c38dd31dc23
김정민 <kjungmin0601@gmail.com>
2023-06-06 19:22:13 +0900

    chore 서블릿 템플릿엔진 로거 테스트 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,12 @@ dependencies {
     // 서버 실행 시키기용 톰켓
     implementation("org.apache.tomcat.embed:tomcat-embed-core:10.1.7")
     implementation("org.apache.tomcat.embed:tomcat-embed-jasper:10.1.7")
+    // 서블릿
+    implementation("javax.servlet:javax.servlet-api:4.0.1")
+    // JSP
+    implementation("javax.servlet.jsp.jstl:jstl:1.2")
+    // 로거
+    implementation("ch.qos.logback:logback-classic:1.4.7")
 
     testImplementation(kotlin("test"))
 }
---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+
+class MainTest {
+
+    @Test
+    fun test() {
+        println("test ok")
+    }
+}
